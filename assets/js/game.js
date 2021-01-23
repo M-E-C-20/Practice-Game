3546726e99a9115ab5cb8d380dd979f5db732f36
M-E-C-20 <mcasey8511@gmail.com>
2021-01-23 02:16:41 +0000

    add movemnt and bounce feature to red ball

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,16 +1,36 @@
 var canvas;
 var canvasContext;
+var ballX = 0;
+var ballSpeedX = 20;
 
 window.onload = function () {
-    console.log('hi');
     canvas = document.getElementById('game-canvas');
     canvasContext = canvas.getContext('2d');
-    canvasContext.fillStyle = 'black';
-    canvasContext.fillRect(0, 0, canvas.width, canvas.height);
-    canvasContext.fillStyle = 'red';
-    canvasContext.fillRect(100, 200, 60, 60);
-    canvasContext.fillStyle = 'green';
-    canvasContext.fillRect(200, 200, 60, 30);
-    canvasContext.fillStyle = 'white';
-    canvasContext.fillRect(400 - 30, 300 - 30, 60, 60);
+
+    var framesPerSecond = 30;
+    setInterval(function () {
+        moveEverything();
+        drawEverything();
+    }, 1000 / framesPerSecond);
 };
+
+// this controls the ball position
+function moveEverything() {
+    ballX = ballX + ballSpeedX;
+    if (ballX == canvas.height - 20 || ballX == 0) {
+        ballSpeedX = -ballSpeedX;
+    }
+}
+
+// this draws the objects
+function drawEverything() {
+    colorRect(0, 0, canvas.width, canvas.height, 'black');
+    colorRect((canvas.width / 2) - 20, ballX, 20, 20, 'red');
+    colorRect(0, canvas.height - 50, 100, 50, 'orange');
+}
+
+// this was used to reduce the number of lines of code in the drawEverything function
+function colorRect(x, y, width, height, drawColor) {
+    canvasContext.fillStyle = drawColor;
+    canvasContext.fillRect(x, y, width, height);
+}
